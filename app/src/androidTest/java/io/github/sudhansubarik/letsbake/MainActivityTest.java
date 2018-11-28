a9b142544dc882b3c08c5a8973598391e0518be1
Sudhansu Barik <sudhansubarik07@gmail.com>
2018-11-28 23:52:02 +0530

    Updated Espresso Test

--- app/src/androidTest/java/io/github/sudhansubarik/letsbake/MainActivityTest.java
+++ app/src/androidTest/java/io/github/sudhansubarik/letsbake/MainActivityTest.java
@@ -1,60 +1,46 @@
 package io.github.sudhansubarik.letsbake;
 
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import io.github.sudhansubarik.letsbake.main.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    @Test
+    public void clickItemOne() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        // Click item at 1st position
+        onView(withId(R.id.menu_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
 
     @Test
-    public void mainActivityTest() {
-        ViewInteraction recyclerView = onView(allOf(withId(R.id.menu_list),
-                childAtPosition(withClassName(is("android.widget.FrameLayout")), 0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+    public void clickItemFour() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Click item at 4th position
+        onView(withId(R.id.menu_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
     }
 }
